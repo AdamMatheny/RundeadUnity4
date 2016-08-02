3a6f9edbec7e86e719ad2c7f4c4628554e3ef2d8
sukirozla <twinzmcneal@gmail.com>
2016-08-02 14:52:08 -0400

    test push

---
+++ Assets/Editor/CircuitPathEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(CircuitPath))]
+public class CircuitPathEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		CircuitPath myScript = (CircuitPath)target;
+		
+	//EditorGUILayout.IntField("Number of Joints", target.thePath)
+
+		if(GUILayout.Button("Add Joint"))
+		{
+			myScript.SpawnJoint();
+		}
+	}
+}
+
---
+++ Assets/Editor/SwitchEditor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using Assets.CustomScripts;
+
+[CustomEditor(typeof(SwitchToggler))]
+public class SwitchEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		SwitchToggler myScript = (SwitchToggler)target;
+		if (GUILayout.Button("Add Path"))
+		{
+			if(myScript.Paths.Count < myScript.LinkedObjects.Length)
+			{
+				myScript.AddPath();
+			}
+
+		}
+		//SwitchToggler myScript = (SwitchToggler)target;
+		//myScript.Type = (Assets.CustomScripts.SwitchType)EditorGUILayout.EnumPopup("Type", (System.Enum)myScript.Type);
+		//switch(myScript.Type)
+		//{
+		//	case SwitchType.Toggle:
+		//		ToggleSelected();
+		//		break;
+		//	case SwitchType.Timed:
+		//		TimedSelected();
+		//		break;
+		//	case SwitchType.Pressure:
+		//		PressureSelected();
+		//		break;
+		//	default:
+		//		Debug.Log("Error: Switch CustomEditor has more selected than needed");
+		//		break;
+		//}
+	}
+
+	private void ToggleSelected()
+	{
+	}
+	private void TimedSelected()
+	{
+	}
+	private void PressureSelected()
+	{
+	}
+}
