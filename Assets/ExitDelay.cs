dc7ca2ddd9c285ce15fb08c5f7d6b24d7ed72632
Zach Johnson <johnsonzacharye@gmail.com>
2016-11-30 21:12:08 -0500

    This script delays the exit in level 3, separated.

---
+++ Assets/ExitDelay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitDelay : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		StartCoroutine(finalWord ());
+	}
+	
+	IEnumerator finalWord() {
+		yield return new WaitForSeconds(3);
+		Application.LoadLevel(Application.loadedLevel+1);
+	}
+}
+
+
+
+
+	
